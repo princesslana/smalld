4a9990f8806fa508968f29c95195af1688e592c5
ianagbip1oti <ianagbip1oti@gmail.com>
2020-05-30 05:12:58 +0000

    ✅ TEST: Refactoring of listener tests

--- src/test/java/com/github/princesslana/smalld/TestIdentify.java
+++ src/test/java/com/github/princesslana/smalld/TestIdentify.java
@@ -5,44 +5,24 @@ import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 import net.javacrumbs.jsonunit.assertj.JsonAssertions;
 import org.awaitility.Awaitility;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
-public class TestIdentify {
-
-  private static final String MOCK_TOKEN = "Mock.Token";
-
-  private Identify subject;
-
-  @Mock private SmallD smalld;
+public class TestIdentify extends ListenerTest<Identify> {
 
   @Mock private SequenceNumber sequenceNumber;
 
-  private Consumer<String> listener;
-
-  @BeforeEach
-  public void subject() {
-    subject = new Identify(sequenceNumber);
-
-    listener = capturePayloadListener(subject);
-
-    Mockito.lenient().when(smalld.getToken()).thenReturn(MOCK_TOKEN);
-    Mockito.lenient().when(smalld.getCurrentShard()).thenReturn(0);
-    Mockito.lenient().when(smalld.getNumberOfShards()).thenReturn(1);
+  @Override
+  protected Identify createListener() {
+    return new Identify(sequenceNumber);
   }
 
   @Test
   public void whenReceiveHello_shouldSendIdentify() {
-    listener.accept(Json.object().add("op", GatewayPayload.OP_HELLO).toString());
+    sendToListener(Json.object().add("op", GatewayPayload.OP_HELLO));
 
     String sent = captureSentPayload();
 
@@ -61,14 +41,13 @@ public class TestIdentify {
   public void whenReceiveHelloAfterReady_shouldSendResume() {
     Mockito.when(sequenceNumber.getLastSeen()).thenReturn(Optional.of(42L));
 
-    listener.accept(
+    sendToListener(
         Json.object()
             .add("op", GatewayPayload.OP_DISPATCH)
             .add("t", "READY")
-            .add("d", Json.object().add("session_id", "abc123"))
-            .toString());
+            .add("d", Json.object().add("session_id", "abc123")));
 
-    listener.accept(Json.object().add("op", GatewayPayload.OP_HELLO).toString());
+    sendToListener(Json.object().add("op", GatewayPayload.OP_HELLO));
 
     String sent = captureSentPayload();
 
@@ -84,36 +63,11 @@ public class TestIdentify {
   public void whenReceiveInvalidSession_shouldWaitAndIdentify()
       throws InterruptedException, ExecutionException {
     CompletableFuture.runAsync(
-        () ->
-            listener.accept(Json.object().add("op", GatewayPayload.OP_INVALID_SESSION).toString()));
+        () -> sendToListener(Json.object().add("op", GatewayPayload.OP_INVALID_SESSION)));
 
     CompletableFuture<String> sent = awaitSentPayload();
 
     Awaitility.await().atLeast(1, TimeUnit.SECONDS).atMost(5, TimeUnit.SECONDS).until(sent::isDone);
     JsonAssertions.assertThatJson(sent.get()).node("op").isEqualTo(GatewayPayload.OP_IDENTIFY);
-  }
-
-  private Consumer<String> capturePayloadListener(Consumer<SmallD> bot) {
-    @SuppressWarnings("unchecked")
-    ArgumentCaptor<Consumer<String>> listener = ArgumentCaptor.forClass(Consumer.class);
-
-    bot.accept(smalld);
-    Mockito.verify(smalld).onGatewayPayload(listener.capture());
-    return listener.getValue();
-  }
-
-  private String captureSentPayload() {
-    ArgumentCaptor<String> sent = ArgumentCaptor.forClass(String.class);
-    Mockito.verify(smalld).sendGatewayPayload(sent.capture());
-    return sent.getValue();
-  }
-
-  private CompletableFuture<String> awaitSentPayload() {
-    return CompletableFuture.supplyAsync(
-        () -> {
-          ArgumentCaptor<String> sent = ArgumentCaptor.forClass(String.class);
-          Mockito.verify(smalld, Mockito.timeout(10000)).sendGatewayPayload(sent.capture());
-          return sent.getValue();
-        });
   }
 }
--- src/test/java/com/github/princesslana/smalld/TestSequenceNumber.java
+++ src/test/java/com/github/princesslana/smalld/TestSequenceNumber.java
@@ -1,59 +1,40 @@
 package com.github.princesslana.smalld;
 
 import com.eclipsesource.json.Json;
-import java.util.function.Consumer;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class TestSequenceNumber {
+public class TestSequenceNumber extends ListenerTest<SequenceNumber> {
 
-  private SequenceNumber subject;
-
-  @Mock private SmallD smalld;
-
-  @Captor private ArgumentCaptor<Consumer<String>> listener;
-
-  @BeforeEach
-  public void subject() {
-    subject = new SequenceNumber();
-    subject.accept(smalld);
-
-    Mockito.verify(smalld).onGatewayPayload(listener.capture());
+  @Override
+  protected SequenceNumber createListener() {
+    return new SequenceNumber();
   }
 
   @Test
   public void getLastSeen_whenInitialized_shouldBeAbsent() {
-    Assertions.assertThat(subject.getLastSeen()).isEmpty();
+    Assertions.assertThat(getListener().getLastSeen()).isEmpty();
   }
 
   @Test
   public void getLastSeen_whenNoSequenceNumber_shouldBeAbsent() {
-    sendPayload(Json.object().add("op", 0).toString());
-    Assertions.assertThat(subject.getLastSeen()).isEmpty();
+    sendToListener(Json.object().add("op", 0));
+    Assertions.assertThat(getListener().getLastSeen()).isEmpty();
   }
 
   @Test
   public void getLastSeen_whenSequenceNumber_shouldBeThatNumber() {
-    sendPayload(Json.object().add("s", 42).toString());
-    Assertions.assertThat(subject.getLastSeen()).isPresent().contains(42L);
+    sendToListener(Json.object().add("s", 42));
+    Assertions.assertThat(getListener().getLastSeen()).isPresent().contains(42L);
   }
 
   @Test
   public void getLastSeen_whenNullSequenceNumber_shouldBePreviousNumber() {
-    sendPayload(Json.object().add("s", 42).toString());
-    sendPayload(Json.object().add("s", Json.NULL).toString());
-    Assertions.assertThat(subject.getLastSeen()).isPresent().contains(42L);
-  }
-
-  private void sendPayload(String payload) {
-    listener.getValue().accept(payload);
+    sendToListener(Json.object().add("s", 42));
+    sendToListener(Json.object().add("s", Json.NULL));
+    Assertions.assertThat(getListener().getLastSeen()).isPresent().contains(42L);
   }
 }
